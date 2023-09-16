99ca3e561bb2876076ec11bf66155bf226718f76
Noah Shoap <nshoap@vols.utk.edu>
2023-09-15 21:49:08 -0400

    search works

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
-const apiStr = "http://localhost:5091/api/inventory/get";
+const inventory = document.querySelector(".inventory");
+const searchButton = document.querySelector(".search-button");
+const search = document.querySelector("#search");
 
 function addItem(item)
 {
@@ -21,8 +23,42 @@ function addItem(item)
     list.appendChild(itemElement);
 }
 
+// Removes previous search results.
+function clearResults()
+{
+    while (inventory.firstChild) {
+        inventory.removeChild(inventory.firstChild);
+    }
+}
+
+// Gets items of a certain name.
+function searchInventory()
+{
+    clearResults();
+    const apiStr = "http://localhost:5091/api/inventory/search?itemName=" + search.value;
+
+    fetch (apiStr)
+        .then((response) => {
+            if (!response.ok)
+                throw new Error(`Failed to find any items containg ${search.textContent}`);
+
+            return response.json();
+        })
+        .then((data) => {
+            data.forEach((item) => {
+                addItem(item);
+            })
+        })
+        .catch((error) => {
+            console.error(error);
+        })
+}
+
+// Gets the entire inventory.
 function getInventory()
 {
+    const apiStr = "http://localhost:5091/api/inventory/get";
+
     fetch (apiStr)
         .then((response) => {
             if (!response.ok)
@@ -40,4 +76,4 @@ function getInventory()
         });
 }
 
-getInventory();
+searchButton.addEventListener("click", searchInventory);
